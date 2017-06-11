185474872119f14302754317eed7a28884c4c2c6
Lxuanx <627564056@qq.com>
2017-06-11 18:35:59 +0800

    第 4 次提交  浸入式的修改

--- app/src/main/java/com/qiyi/openapi/demo/activity/BaseActivity.java
+++ app/src/main/java/com/qiyi/openapi/demo/activity/BaseActivity.java
@@ -1,10 +1,11 @@
 package com.qiyi.openapi.demo.activity;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 import com.qiyi.openapi.demo.R;
@@ -16,6 +17,15 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //状态栏和导航栏透明化
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                getWindow().setStatusBarColor(getResources().getColor(android.R.color.transparent));
+            }
+        }
+
         mRootView = LayoutInflater.from(this).inflate(getLayoutResourceId(), null, false);
         setContentView(mRootView);
         initView();
--- app/src/main/java/com/qiyi/openapi/demo/activity/PlayerActivity.java
+++ app/src/main/java/com/qiyi/openapi/demo/activity/PlayerActivity.java
@@ -169,10 +169,9 @@ public class PlayerActivity extends BaseActivity {
      * @return
      */
     private String ms2hms(int millis) {
-        String result = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
+        return String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
                 TimeUnit.MILLISECONDS.toMinutes(millis) % TimeUnit.HOURS.toMinutes(1),
                 TimeUnit.MILLISECONDS.toSeconds(millis) % TimeUnit.MINUTES.toSeconds(1));
-        return result;
     }
 
     IQYPlayerHandlerCallBack mCallBack = new IQYPlayerHandlerCallBack() {
